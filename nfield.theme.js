ed86f3e6cab581a410c8d36ac98f103d88ea8e30
Rutger de Jong <r.de.jong@kantar.com>
2021-10-26 10:31:16 +0200

    Add Azure lib

--- nfield.theme.js
+++ nfield.theme.js
@@ -4,6 +4,9 @@ var theme = {} || theme;
 theme.init = function () {
 
     // init azure library
+    $.getScript("https://cdn.jsdelivr.net/npm/microsoft-cognitiveservices-speech-sdk@latest/distrib/browser/microsoft.cognitiveservices.speech.sdk.bundle-min.js", function( data, textStatus, jqxhr ) {
+        $("<p>Azure cognitive services init: " + textStatus + "</p>").insertAfter(".segment.active h2");
+      });
 
     // azure API key
 
