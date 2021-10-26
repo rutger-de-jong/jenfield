8ff0797f8f525d85d65f177f34acc09a2870a8d2
Rutger de Jong <r.de.jong@kantar.com>
2021-10-26 12:10:39 +0200

    Read out question text

--- nfield.theme.js
+++ nfield.theme.js
@@ -1,15 +1,45 @@
 var theme = {} || theme;
 
-
 theme.init = function () {
 
-    // init azure library
-    $.getScript("https://cdn.jsdelivr.net/npm/microsoft-cognitiveservices-speech-sdk@latest/distrib/browser/microsoft.cognitiveservices.speech.sdk.bundle-min.js", function( data, textStatus, jqxhr ) {
-        $("<p>Azure cognitive services init: " + textStatus + "</p>").insertAfter(".segment.active h2");
-      });
+    console.log = function(data) {
+        $("<p>Console: " + data + "</p>").insertAfter(".segment.active h2");
+    }
 
-    // azure API key
+    var currentPath = $('script[src*=theme]').attr('src');
+    currentPath = currentPath.replace(/nfield.theme.js*$/, '');
+    $.getScript(currentPath + "dist/js/microsoft.cognitiveservices.speech.sdk.bundle-min.js").done(function() {
+        console.log("Azure loading successful");
 
+        const speechConfig = SpeechSDK.SpeechConfig.fromSubscription("cff004e326644706a97b025c19495f4d", "westeurope");
+        speechConfig.speechSynthesisLanguage = "en-US";
+        speechConfig.speechSynthesisVoiceName = "en-US-JennyNeural";
+
+        const audioConfig = SpeechSDK.AudioConfig.fromDefaultSpeakerOutput();
+
+        const synthesizer = new SpeechSDK.SpeechSynthesizer(speechConfig, audioConfig);
+
+        let questionText = $(".segment.active h2").text().trim();
+
+        synthesizer.speakTextAsync(
+            questionText,
+            result => {
+                if (result) {
+                    synthesizer.close();
+                    return result.audioData;
+                }
+            },
+            error => {
+                console.log(error);
+                synthesizer.close();
+            });
+
+
+    }).fail(function() {
+        console.log("Failed to open Cognitive Services: " + currentPath);
+    });      
+    
     // hide answer container
 
-}; 
+};
+
