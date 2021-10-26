05799c1e48a375dda7bfba9b7013de0492255644
Martin Rijks <M.Rijks@kantar.com>
2021-10-26 15:37:51 +0200

    It works!

--- nfield.theme.js
+++ nfield.theme.js
@@ -1,4 +1,7 @@
 var theme = {} || theme;
+var recognizer;
+var speechConfig;
+var lastText ="";
 
 theme.init = function () {
 
@@ -10,7 +13,7 @@ theme.init = function () {
     currentPath = currentPath.replace(/nfield.theme.js*$/, '');
     $.getScript(currentPath + "dist/js/microsoft.cognitiveservices.speech.sdk.bundle-min.js").done(function() {
     
-        const speechConfig = SpeechSDK.SpeechConfig.fromSubscription("cff004e326644706a97b025c19495f4d", "westeurope");
+        speechConfig = SpeechSDK.SpeechConfig.fromSubscription("cff004e326644706a97b025c19495f4d", "westeurope");
         speechConfig.speechSynthesisLanguage = "en-US";
         speechConfig.speechSynthesisVoiceName = "en-US-JennyNeural";
         
@@ -39,31 +42,53 @@ theme.init = function () {
         var sttAudioConfig  = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
         recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, sttAudioConfig);
 
-        recognizer.recognizeOnceAsync(
-          function (result) {
+        recognizer.startContinuousRecognitionAsync();
+
+        function onRecognizing(sender, recognitionEventArgs) {
+            var result = recognitionEventArgs.result;
+
+            // Update the hypothesis line in the phrase/result view (only have one)
+            let oldText = answerTextarea.val();
+            answerTextarea.val(oldText.replace(/(.*)(^|[\r\n]+).*\[\.\.\.\][\r\n]+/, '$1$2')
+                + `${result.text} [...]\r\n`);
+            answerTextarea.scrollTop = answerTextarea.scrollHeight;
+        }
+
+        function onRecognized(sender, recognitionEventArgs) {
+            var result = recognitionEventArgs.result;
+            onRecognizedResult(recognitionEventArgs.result);
+        }
+
+        function onRecognizedResult(result) {
+            answerTextarea.scrollTop = answerTextarea.scrollHeight;
 
             let oldText = answerTextarea.val();
-            
-            answerTextarea.val(oldText + result.text)
-            
-            recognizer.close();
-            recognizer = undefined;
-          },
-          function (err) {
-            
-            answerTextarea.val(oldText + result.text)
-            window.console.log(err);
+            answerTextarea.val(oldText.replace(/(.*)(^|[\r\n]+).*\[\.\.\.\][\r\n]+/, '$1$2'));
 
-            recognizer.close();
-            recognizer = undefined;
-        });
+            switch (result.reason) {
 
+                case SpeechSDK.ResultReason.RecognizedSpeech:
+                case SpeechSDK.ResultReason.RecognizedIntent:
+
+                    if (result.text) {
+                        oldText = answerTextarea.val();
+                        answerTextarea.val(oldText + `${result.text}\r\n`);
+                    }
+                    break;
+            }
+        }
+
+        
+        recognizer.recognizing = onRecognizing;
+        recognizer.recognized = onRecognized;
 
     }).fail(function() {
         console.log("Failed to open Cognitive Services: " + currentPath);
-    });      
-    
-    // hide answer container
+    });
 
+    window.addEventListener('unload', function(event) {
+        recognizer.close();
+        recognizer = undefined;
+    });
 };
 
