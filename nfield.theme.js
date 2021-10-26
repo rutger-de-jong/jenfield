e4f34b22539b4b5b6e34f3c8b7adb0a7a0c52023
Rutger de Jong <r.de.jong@kantar.com>
2021-10-26 13:37:24 +0200

    Add speech recognition

--- nfield.theme.js
+++ nfield.theme.js
@@ -9,17 +9,18 @@ theme.init = function () {
     var currentPath = $('script[src*=theme]').attr('src');
     currentPath = currentPath.replace(/nfield.theme.js*$/, '');
     $.getScript(currentPath + "dist/js/microsoft.cognitiveservices.speech.sdk.bundle-min.js").done(function() {
-        console.log("Azure loading successful");
-
+    
         const speechConfig = SpeechSDK.SpeechConfig.fromSubscription("cff004e326644706a97b025c19495f4d", "westeurope");
         speechConfig.speechSynthesisLanguage = "en-US";
         speechConfig.speechSynthesisVoiceName = "en-US-JennyNeural";
+        
+        const ttsAudioConfig = SpeechSDK.AudioConfig.fromDefaultSpeakerOutput();
 
-        const audioConfig = SpeechSDK.AudioConfig.fromDefaultSpeakerOutput();
-
-        const synthesizer = new SpeechSDK.SpeechSynthesizer(speechConfig, audioConfig);
+        const synthesizer = new SpeechSDK.SpeechSynthesizer(speechConfig, ttsAudioConfig);
 
         let questionText = $(".segment.active h2").text().trim();
+
+        let answerTextarea = $(".answerCategory .form-control.open");
 
         synthesizer.speakTextAsync(
             questionText,
@@ -30,9 +31,32 @@ theme.init = function () {
                 }
             },
             error => {
-                console.log(error);
+                window.console.log(error);
                 synthesizer.close();
             });
+
+        speechConfig.speechRecognitionLanguage = "en-US";
+        var sttAudioConfig  = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
+        recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, sttAudioConfig);
+
+        recognizer.recognizeOnceAsync(
+          function (result) {
+
+            let oldText = answerTextarea.val();
+            
+            answerTextarea.val(oldText + result.text)
+            
+            recognizer.close();
+            recognizer = undefined;
+          },
+          function (err) {
+            
+            answerTextarea.val(oldText + result.text)
+            window.console.log(err);
+
+            recognizer.close();
+            recognizer = undefined;
+        });
 
 
     }).fail(function() {
--- questionnaire.odin
+++ questionnaire.odin
@@ -3,4 +3,10 @@
 *QUESTION 10 *OPEN 61L1
 What did you do last weekend?
 
+*QUESTION 20 *CODES 62L1
+Do you like trains?
+
+1:Yes
+2:No
+
 *END
